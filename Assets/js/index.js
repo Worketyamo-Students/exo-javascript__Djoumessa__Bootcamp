3c39ead0e7cd6cd0a96ce47922b1d56d3c6afbfa
K9e7l6l7y <megniguimkelly@gmail.coccom>
2026-08-22 08:40:26 +0100

    exo-6

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,1 +1,13 @@
 // Question 6 — Accès à une plateforme
+let isConnected;
+let isActive;
+let isAdmin;
+isConnected = false;
+isActive = false;
+isAdmin = true;
+
+if((isActive === true && isActive == true) || (isAdmin === true)){
+    alert(" l'accès doit être autorisé.");
+}else{
+    alert(" accès non autorisé.");
+}
